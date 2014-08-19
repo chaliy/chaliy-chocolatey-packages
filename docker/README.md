c3b6eb54400bed4c650b42910438deffc5d29568
Mike Chaliy <mike@chaliy.name>
2014-08-19 10:36:46 +0300

    Docker 1.1.2

--- docker/README.md
+++ docker/README.md
@@ -2,3 +2,6 @@ docker-chocolatey
 ==================
 
 Boot2Docker package for Chocolatey
+
+Source: http://boot2docker.io/ 
+Releases: https://github.com/boot2docker/windows-installer/releases
--- docker/tools/chocolateyInstall.ps1
+++ docker/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-Install-ChocolateyPackage 'docker' 'exe' '/VERYSILENT' 'https://github.com/boot2docker/windows-installer/releases/download/v1.1.1/docker-install.exe'
+Install-ChocolateyPackage 'docker' 'exe' '/VERYSILENT' 'https://github.com/boot2docker/windows-installer/releases/download/v1.1.2/docker-install.exe'
 
 Install-ChocolateyPath "$env:ProgramFiles\Boot2Docker for Windows\"

b5e5c65550a0bb4b0835bb2f278d1a8c4efb1f20
Mike Chaliy <mike@chaliy.name>
2014-11-26 21:51:02 +0200

    Boot2Docker updated to 1.3.2

--- activator/tools/chocolateyInstall.ps1
+++ activator/tools/chocolateyInstall.ps1
@@ -1,4 +1,26 @@
 $here = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
+function Install-BinFile {
+  param(
+    [string] $name,
+    [string] $path
+  )
+
+  Write-Debug "Running 'Install-BinFile' for $name with path:`'$path`'";
+
+  $packageBatchFileName = Join-Path $nugetExePath "$name.bat"
+
+  if (Test-Path ($packageBatchFileName)) {Remove-Item $packageBatchFileName -force}
+  $path = $path.ToLower().Replace($nugetPath.ToLower(), "..\").Replace("\\","\")
+  $path = "%DIR%$($path)"
+  $pathBash = $path.Replace("%DIR%..\","`$DIR/../").Replace("\","/")
+  Write-Host "Adding $packageBatchFileName and pointing to `'$path`'." -ForegroundColor $Note
+"@echo off
+SET DIR=%~dp0%
+cmd /c ""$path %*""
+exit /b %ERRORLEVEL%" | Out-File $packageBatchFileName -encoding ASCII
+}
+
+
 # Install apm shortcut
 Install-BinFile "activator" "$here\activator.bat"

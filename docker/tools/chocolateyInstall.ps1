e1e906dc4a361a713f3fdbcfac0b8433211245fd
Mike Chaliy <mike@chaliy.name>
2015-01-20 10:19:00 +0200

    Added 1.4.1 of boot2docker #2

--- docker/tools/chocolateyInstall.ps1
+++ docker/tools/chocolateyInstall.ps1
@@ -1,3 +1,5 @@
+# https://github.com/chaliy/chaliy-chocolatey-packages/
+
 Install-ChocolateyPackage 'docker' 'exe' '/VERYSILENT' 'https://github.com/boot2docker/windows-installer/releases/download/v1.4.1/docker-install.exe'
 
 Install-ChocolateyPath "$env:ProgramFiles\Boot2Docker for Windows\"

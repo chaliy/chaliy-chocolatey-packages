78a68737d149666d33876e0083f410b52d6e637e
Mike Chaliy <mike@chaliy.name>
2015-03-11 11:14:47 +0200

    Added rust 1.0.0-alpha.2

--- rust/tools/chocolateyInstall.ps1
+++ rust/tools/chocolateyInstall.ps1
@@ -1,1 +1,4 @@
-Install-ChocolateyPackage 'rust' 'exe' '/VERYSILENT' 'https://static.rust-lang.org/dist/rust-0.12.0-x86_64-w64-mingw32.exe'
+$Url = 'https://static.rust-lang.org/dist/rust-1.0.0-alpha.2-i686-pc-windows-gnu.exe'
+$Url64 = 'https://static.rust-lang.org/dist/rust-1.0.0-alpha.2-x86_64-pc-windows-gnu.exe'
+
+Install-ChocolateyPackage 'rust' 'exe' '/VERYSILENT' "$Url" "$Url64"

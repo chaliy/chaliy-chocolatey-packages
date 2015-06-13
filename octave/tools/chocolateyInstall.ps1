5fbad37874fbd6c929ff5629e1f084468eed443f
Mike Chaliy <mike@chaliy.name>
2015-06-13 14:06:30 +0300

    Added 4.0 of octave, now MXE build

--- octave/tools/chocolateyInstall.ps1
+++ octave/tools/chocolateyInstall.ps1
@@ -1,19 +1,8 @@
-if(-not($Env:ChocolateyBinRoot)) {
-  $BinRoot = "$Env:ChocolateyInstall\bin"
-} else {
-  $BinRoot = $Env:ChocolateyBinRoot
-}
-if (-not($BinRoot -imatch "^\w:")) {
-    $BinRoot = Join-Path $Env:SystemDrive $BinRoot
-}
-
 $packageName = 'Octave'
 $installerType = 'EXE'
-$url = 'http://downloads.sourceforge.net/project/octave/Octave%20Windows%20binaries/Octave%203.6.4%20for%20Windows%20Microsoft%20Visual%20Studio/octave-3.6.4-vs2010-setup.exe'
+$url = 'https://ftp.gnu.org/gnu/octave/windows/octave-4.0.0_0-installer.exe'
 $url64 = $url
-$silentArgs = "/S /D=$($BinRoot)Octave\"
+$silentArgs = "/S"
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-
-Install-ChocolateyPath "$($BinRoot)Octave\" 'Machine'

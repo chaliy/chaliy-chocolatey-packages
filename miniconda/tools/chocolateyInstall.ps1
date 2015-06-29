8dc5b41902a2091253ea76d6749e476a6fe156b5
Mike Chaliy <mike@chaliy.name>
2015-06-29 11:37:17 +0300

    Version 3.10.1 of Miniconda

--- miniconda/tools/chocolateyInstall.ps1
+++ miniconda/tools/chocolateyInstall.ps1
@@ -1,10 +1,8 @@
-$BinRoot = Get-BinRoot
-
 $PackageName = 'miniconda'
 $InstallerType = 'EXE'
-$Url = 'https://repo.continuum.io/miniconda/Miniconda-3.9.1-Windows-x86.exe'
-$Url64 = 'https://repo.continuum.io/miniconda/Miniconda-3.9.1-Windows-x86_64.exe'
-$Args = "/InstallationType=AllUsers /S /D=$BinRoot\Miniconda\"
+$Url = 'https://repo.continuum.io/miniconda/Miniconda-3.10.1-Windows-x86.exe'
+$Url64 = 'https://repo.continuum.io/miniconda/Miniconda-3.10.1-Windows-x86_64.exe'
+$Args = "/InstallationType=AllUsers /S"
 $ValidExitCodes = @(0)
 
 Install-ChocolateyPackage "$PackageName" "$InstallerType" "$Args" "$Url" "$Url64"  -validExitCodes $ValidExitCodes
--- miniconda3/tools/chocolateyInstall.ps1
+++ miniconda3/tools/chocolateyInstall.ps1
@@ -1,10 +1,8 @@
-$BinRoot = Get-BinRoot
-
 $PackageName = 'miniconda3'
 $InstallerType = 'EXE'
-$Url = 'https://repo.continuum.io/miniconda/Miniconda3-3.9.1-Windows-x86.exe'
-$Url64 = 'https://repo.continuum.io/miniconda/Miniconda3-3.9.1-Windows-x86_64.exe'
-$Args = "/InstallationType=AllUsers /S /D=$BinRoot\Miniconda3\"
+$Url = 'https://repo.continuum.io/miniconda/Miniconda3-3.10.1-Windows-x86.exe'
+$Url64 = 'https://repo.continuum.io/miniconda/Miniconda3-3.10.1-Windows-x86_64.exe'
+$Args = "/InstallationType=AllUsers /S"
 $ValidExitCodes = @(0)
 
 Install-ChocolateyPackage "$PackageName" "$InstallerType" "$Args" "$Url" "$Url64"  -validExitCodes $ValidExitCodes

2925f23a3f80a9a49f4b4d7319a6f3079cef2eac
Friedrich von Never <friedrich@fornever.me>
2015-07-05 16:15:10 +0600

    Rust 1.1.0.

--- rust/tools/chocolateyInstall.ps1
+++ rust/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $PackageName = 'rust'
 $InstallerType = 'msi'
-$Url = 'https://static.rust-lang.org/dist/rust-1.0.0-i686-pc-windows-gnu.msi'
-$Url64 = 'https://static.rust-lang.org/dist/rust-1.0.0-x86_64-pc-windows-gnu.msi'
+$Url = 'https://static.rust-lang.org/dist/rust-1.1.0-i686-pc-windows-gnu.msi'
+$Url64 = 'https://static.rust-lang.org/dist/rust-1.1.0-x86_64-pc-windows-gnu.msi'
 $SilentArgs = '/quiet'
 $ValidExitCodes = @(0,3010)
 
